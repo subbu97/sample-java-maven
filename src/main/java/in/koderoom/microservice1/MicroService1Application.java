af6b3a5cd83a53e4310dbaf62ab0cecbaeb46f03
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-13 16:25:41 +0530

    swagger in progress.

--- src/main/java/in/koderoom/microservice1/MicroService1Application.java
+++ src/main/java/in/koderoom/microservice1/MicroService1Application.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -26,7 +27,21 @@ public class MicroService1Application {
 				.select()
 				.paths(PathSelectors.ant("/api/*"))
 				.apis(RequestHandlerSelectors.basePackage("in.koderoom"))
-				.build();
+				.build()
+				.apiInfo(apiMetaInfo());
+	}
+
+	private ApiInfo apiMetaInfo() {
+		return new ApiInfo(
+				"Koderoom Book API",
+				"Api description, koderoom book.",
+				"1.0",
+				"Free and Open Source",
+				new springfox.documentation.service.Contact("Santosh", "https://koderoom.tech","santosh@gamil.com"),
+				"Free and Open Source",
+				"https://koderoom.tech",
+				Collections.emptyList()
+		);
 	}
 
 
